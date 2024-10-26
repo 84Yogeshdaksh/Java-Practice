62ef21f69d12f7ae0074e62168b0cb4ed3117afd
Yogesh Daksh <Yogeshdaksh606@gmail.com>
2024-10-26 16:57:57 +0530

    Exception

---
+++ exception.java
@@ -0,0 +1,19 @@
+public class exception {
+
+    public static void main(String[] args) {
+        int a[]={22,233,3};
+        try{
+            System.out.println("vlaue is: "+a[4]);
+        }
+        catch(IndexOutOfBoundsException e){
+            System.out.println("error occure");
+        }
+        System.out.println("code run sucesfully........");
+
+
+        } 
+        
+
+       
+        
+    }
